c9e97d8c93d44f61c2b722ecb2b7352538128b90
KaporalClems <clement.tossam.dsr@gmail.com>
2025-07-16 11:28:21 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-const CACHE_NAME = 'destination-sud-reunion-v1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'destination-sud-cache-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/app.js',
+  '/style.css',
   '/manifest.webmanifest',
-  '/assets/logo.png',
   '/assets/icon-192.png',
   '/assets/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
